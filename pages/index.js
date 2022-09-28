3aca1d0ab32f679fab20658dde9c60e734d998b4
Karen Dickenson <karen.dickenson@ten7.com>
2022-09-28 14:43:33 -0400

    correction of className on span in index, style change

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Let's see if branching and running a PR will trigger a build.</p>
-        <p>Welcome to Karen's first <span className="strong">deployed</span> Next.js app. Reach out to me on <a href="https://twitter.com/kldickenson">Twitter</a> to hear about what I have been up to.</p>
+        <p>Welcome to Karen's first <span className={utilStyles.strong}>deployed</span> Next.js app. Reach out to me on <a href="https://twitter.com/kldickenson">Twitter</a> to hear about what I have been up to.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
